c5acbf3847be4c0a18bdc631c447c83a51159268
dg-465 <davidgeorgesiren@gmail.com>
2026-04-09 10:52:26 +0800

    Add configuration for Gladihoppers game

---
+++ script.js
@@ -0,0 +1,15 @@
+window.config = {
+    loader: 'unity',
+    debug: false,
+    maxRatio: 16 / 9,
+    minRatio: 9 / 16,
+    title: 'Gladihoppers',
+    unityVersion: '2019.3.13f1',
+    unityWebglBuildUrl: 'https://cdn.jsdelivr.net/gh/genizy/gh/Build/Gladihoppers.json',
+    fileSize: 23,
+    cachedDecompressedFileSizes: {
+        'Gladihoppers.data.unityweb': 17409938,
+        'Gladihoppers.wasm.code.unityweb': 6410659,
+        'Gladihoppers.wasm.framework.unityweb': 91463,
+    },
+};
